d30014a98b60805a7c48a47798b42fa66b7e374d
Tom Ofek Rytt <tomofekrytt@gmail.com>
2022-01-02 00:31:09 +0200

    fixed icons

--- js/pages/Home.jsx
+++ js/pages/Home.jsx
@@ -1,3 +1,5 @@
+const {NavLink, withRouter} = ReactRouterDOM;
+
 export class Home extends React.Component {
 	render() {
 		return (
@@ -16,28 +18,34 @@ export class Home extends React.Component {
 				<section className='apps-details'>
 					<div className='app-details-mail'>
 						Take a look at our new and unique Mail-App{' '}
-						<img
-							className='mail-app-icon'
-							src='../../assets/imgs/main/email.png'
-							alt=''
-						/>
+						<NavLink activeClassName='my-active' to='/mail'>
+							<img
+								className='mail-app-icon'
+								src='../../assets/imgs/main/email.png'
+								alt=''
+							/>
+						</NavLink>
 					</div>
 					<div className='app-details-notes'>
 						{' '}
 						Try out the Keep-App - noting managing like no other{' '}
-						<img
-							className='note-app-icon'
-							src='../../assets/imgs/main/notes.png'
-							alt=''
-						/>
+						<NavLink activeClassName='my-active' to='/notes'>
+							<img
+								className='note-app-icon'
+								src='../../assets/imgs/main/notes.png'
+								alt=''
+							/>
+						</NavLink>
 					</div>
 					<div className='app-details-books'>
 						Do you like books? everybody does! come on and check our book shop!{' '}
-						<img
-							className='bookshop-icon'
-							src='../../assets/imgs/main/book.png'
-							alt=''
-						/>
+						<NavLink activeClassName='my-active' to='/books'>
+							<img
+								className='bookshop-icon'
+								src='../../assets/imgs/main/book.png'
+								alt=''
+							/>
+						</NavLink>
 					</div>
 				</section>
 				<div className='about flex'>
